a8969be72ec89712737518672adf237de0caef91
William <14932329+WillyChap@users.noreply.github.com>
2025-02-26 07:07:44 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,16 @@
 
 # type "source ./setup.sh" to run this script first
 
-PATH=$(echo "$PATH" | sed -e 's,/glade\/work\/wchapman\/miniconda3/[^:]\+\(:\|$\),,g')
-PATH=$(echo "$PATH" | sed -e 's,/glade\/u\/home\/wchapman\/anaconda/[^:]\+\(:\|$\),,g')
+### example setup script, or if you are using this on NSF-NCAR's Derecho machine:
+#PATH=$(echo "$PATH" | sed -e 's,/glade\/work\/wchapman\/miniconda3/[^:]\+\(:\|$\),,g')
+#PATH=$(echo "$PATH" | sed -e 's,/glade\/u\/home\/wchapman\/anaconda/[^:]\+\(:\|$\),,g')
+#module load conda
+#conda activate /glade/work/wchapman/conda-envs/supermodel_cam
+#export CESM_ROOT=/glade/work/wchapman/supermodel_derecho_conda/
 
+#edit this for your machine:
 module load conda
-conda activate /glade/work/wchapman/conda-envs/supermodel_cam
+conda activate /path/to/your/supermodel_env/
 
-export CESM_ROOT=/glade/work/wchapman/supermodel_derecho_conda/
+export CESM_ROOT=/path/to/your/cesm2.1.5/ROOT/
 chmod +x init_supermodel.py 
